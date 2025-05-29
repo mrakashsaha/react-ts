41efc69cadeafd1cc3646a30cd553918718b2d88
Akash Kumar Saha <mr.akashsaha@gmail.com>
2025-05-29 17:14:06 +0600

    Understanding how we set the Interfae of Props

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Person } from './Person'
+
+function App() {
+
+  return (
+    <>
+      <Person name= {"Akash"} age = {45} isMarried = {true}></Person>
+      <Person name= {"Jamil"} age = {46} isMarried = {false}></Person>
+      <Person name= {"Akash"} age = {45} isMarried = {true}></Person>
+      <Person name={"Tushar"} age={463} isMarried = {false}></Person>
+    </>
+  )
+}
+
+export default App
---
+++ src/Person.tsx
@@ -0,0 +1,19 @@
+interface Props {
+    name: string,
+    age: number,
+    isMarried: boolean,
+
+}
+
+export const Person = (props: Props) => {
+    return(
+        <>
+            <div>
+                <h2>Person</h2>
+                <p>Name: {props.name}</p>
+                <p>Age: {props.age}</p>
+                <p>This person is : {props.isMarried ? "Married" : "Single "}</p>
+            </div>
+        </>
+    )
+}
