1ac999ed8fb3fbc92bbaa5b1d6465e30f01a6f6e
Akash Kumar Saha <mr.akashsaha@gmail.com>
2025-05-31 23:29:26 +0600

    component increased

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ function App() {
       <Person name= {"Jamil"} age = {46} isMarried = {false}></Person>
       <Person name= {"Akash"} age = {45} isMarried = {true}></Person>
       <Person name={"Tushar"} age={463} isMarried = {false}></Person>
+      <Person name={"Tushar"} age={463} isMarried = {false}></Person>
     </>
   )
 }
